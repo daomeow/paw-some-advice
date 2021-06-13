ffc23a8f12ceaff20dd30b43651ad64b63bbdd2a
Melanie Daoheuang <daoh0031@gmail.com>
2021-06-12 21:06:16 -0600

    Build FavoriteCard

--- src/components/FavoriteCard/FavoriteCard.js
+++ src/components/FavoriteCard/FavoriteCard.js
@@ -1,7 +1,12 @@
 import './FavoriteCard.css';
 
-const FavoriteCard = () => {
-
+const FavoriteCard = ({ advice, catImage }) => {
+  return(
+    <div className='favorite-card'>
+      <p>{advice}</p>
+      <img className='cat-picture' src={catImage} alt='Cat picture' id={Date.now()}></img>
+    </div>
+  )
 }
 
 export default FavoriteCard;
--- src/components/FavoriteList/FavoriteList.js
+++ src/components/FavoriteList/FavoriteList.js
@@ -3,33 +3,20 @@ import FavoriteCard from '../FavoriteCard/FavoriteCard';
 import { render } from '@testing-library/react';
 
 const FavoriteList = ({ favorites }) => {  
-  // const favoriteCards = favorites.map(fav => {
-  //   return(
-  //     <div>
-  //       <p>{fav.advice}</p>
-  //       <img className='cat-picture' src={fav.cat} alt='Cat picture' id={Date.now()}></img>
-  //     </div>
-  //   )
-  // })
-  // return(
-  //   <section className='favorite-list'>
-  //     {favoriteCards}
-  //   </section>
-  // )
-
-  render() {
-    const adviceCards = favorites.map(favorite => (
+  const adviceCards = favorites.map(favorite => {
+    return (
       <FavoriteCard
-        advice={favorite.advice}
-        catImage={favorite.cat}
+         advice={favorite.advice}
+         catImage={favorite.cat}
       />
-    ));
-    return(
-      <div className='favorites-container'>
-        {adviceCards}
-      </div>
     )
-  }
+  });
+
+  return (
+    <div className='favorites-container'>
+      {adviceCards}
+    </div>
+  )
 }
 
 export default FavoriteList;
