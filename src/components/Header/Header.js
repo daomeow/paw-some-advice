f29e5940f5b1f5bd79a4ffe76e46e4213bc0eaff
Melanie Daoheuang <daoh0031@gmail.com>
2021-06-13 08:41:04 -0600

    Hide and show home button on correct pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,25 @@
 import './Header.css';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <section className='header'>
       <h1>Paw-some<br/>Advice</h1>
       <section className='tab-container'>
-        <NavLink to='/favorites' className='to-favorites'>
-          <i className="far fa-heart"></i>View Favorites
-        </NavLink>
-        <NavLink to='/' className='go-home'>
-        <i className="fas fa-paw"></i>Go Home
-        </NavLink>
+        {location.pathname !== '/favorites' && 
+          <NavLink to='/favorites' className='to-favorites'>
+            <i className="far fa-heart"></i>View Favorites
+          </NavLink>
+        }
+        {location.pathname === '/favorites' &&
+          <NavLink to='/' className='go-home'>
+            <i className="fas fa-paw"></i>Go Home
+          </NavLink>
+        }
           <a className='game-tab'><i className="fas fa-caret-right"></i>Play Game</a>
+
       </section>
     </section>
   )
