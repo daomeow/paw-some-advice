bf19e5c3485e5de572aa17f920bd8e19a6fec69c
Melanie Daoheuang <daoh0031@gmail.com>
2021-06-12 22:05:43 -0600

    Style nav and button container

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,16 +6,18 @@ function Dashboard ({ advice, catPicture, getData, addFavorite}) {
   return (
     <section className='dashboard'>
       <div className='content'>
+        <p className='advice'>{advice}</p>
         {!catPicture ?
-          <img className='default-cat' src={defaultCat} alt='Default cat photo'></img>
+            <img className='default-cat' src={defaultCat} alt='Default cat photo'></img>
 
           :<div>
-            <p>{advice}</p>
             <img className='cat-picture' src={catPicture} alt='Cat picture'></img>
           </div>
         }
-        <button className='advice-button' onClick={getData}>Receive Advice</button>
-        <button className='add-favorite' onClick={addFavorite}>Add to Favorite</button>
+        <section className='button-container'>
+          <button className='advice-button' onClick={getData}>Receive Advice</button>
+          <button className='add-favorite' onClick={addFavorite}>Add to Favorite</button>
+        </section>
       </div>
     </section>
   )
