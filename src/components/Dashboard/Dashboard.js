9336e76cb06fd4edb5921d659b9a57e96abca1eb
Melanie Daoheuang <daoh0031@gmail.com>
2021-06-12 21:28:38 -0600

    Add container for content

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,16 +5,18 @@ function Dashboard ({ advice, catPicture, getData, addFavorite}) {
 
   return (
     <section className='dashboard'>
-      {!catPicture ?
-        <img className='default-cat' src={defaultCat} alt='Default cat photo'></img>
+      <div className='content'>
+        {!catPicture ?
+          <img className='default-cat' src={defaultCat} alt='Default cat photo'></img>
 
-        :<div>
-          <p>{advice}</p>
-          <img className='cat-picture' src={catPicture} alt='Cat picture'></img>
-        </div>
-      }
-      <button className='advice-button' onClick={getData}>Receive Advice</button>
-      <button className='add-favorite' onClick={addFavorite}>Add to Favorite</button>
+          :<div>
+            <p>{advice}</p>
+            <img className='cat-picture' src={catPicture} alt='Cat picture'></img>
+          </div>
+        }
+        <button className='advice-button' onClick={getData}>Receive Advice</button>
+        <button className='add-favorite' onClick={addFavorite}>Add to Favorite</button>
+      </div>
     </section>
   )
 }
