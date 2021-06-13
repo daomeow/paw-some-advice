5cfd53f6517a7f8da1891d0a22a955bdc0b885c0
Melanie Daoheuang <daoh0031@gmail.com>
2021-06-13 15:26:09 -0600

    Add commands

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,20 +1,15 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+import advice from '../fictures/singleAdvice';
+import catPicture from '../fixtures/singleCat';
 
-// Import commands.js using ES2015 syntax:
-import './commands'
+const adviceBaseURL = 'https://api.adviceslip.com/advice';
+const catBaseURL = 'https://thatcopy.pw/catapi/rest/';
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+Cypress.Commands.add('interceptAdvice', () => {
+  cy.intercept(adviceBaseURL, advice).as('getAdviceStub')
+    .visit('http://localhost:3000/')
+})
+
+Cypress.Commands.add('interceptCatPicture', () => {
+  cy.intercept(catBaseURL, catPicture).as('getCatStub')
+    .visit('http://localhost:3000/')
+})
